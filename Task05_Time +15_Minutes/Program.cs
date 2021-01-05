978cefa028023fb21936bee8f64833a0fc675adc
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 22:22:40 +0200

    Add files via upload

---
+++ Task05_Time +15_Minutes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task05_Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            double hour = double.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int plusMinutes = minutes + 15;
+            //check and output
+            if (plusMinutes < 60)
+            {
+                Console.WriteLine($"{hour}:{plusMinutes}");
+            }
+            else if (plusMinutes == 60)
+            {
+                hour += 1;
+                if (hour == 24) 
+                {
+                  hour = 0;
+                }
+                Console.WriteLine($"{hour}:00");
+            }
+            else if (plusMinutes > 60)
+            {
+                hour += 1;
+                if (hour == 24)
+                {
+                  hour = 0; ;
+                }
+                plusMinutes -= 60;
+                if (plusMinutes < 10)
+                {
+                    Console.WriteLine($"{hour}:0{plusMinutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hour}:{plusMinutes}");
+                }
+            }
+        }
+    }
+}
