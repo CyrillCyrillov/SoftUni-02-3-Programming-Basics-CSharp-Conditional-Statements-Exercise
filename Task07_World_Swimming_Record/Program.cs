e8ac75320f94ad3ab8993e75c1f05ce29e95b3a0
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 22:24:20 +0200

    Add files via upload

---
+++ Task07_World_Swimming_Record/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task07_World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            double worldRecord = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerOneMeter = double.Parse(Console.ReadLine());
+            // math
+            double addSeconds = Math.Floor(distance / 15) * 12.5;
+            double time = distance * timePerOneMeter + addSeconds;
+                        // check and output
+            if (time < worldRecord)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {time:F2} seconds.");
+            }
+            else
+            {
+                double margin = time - worldRecord;
+                Console.WriteLine($"No, he failed! He was {margin:F2} seconds slower.");
+            }
+        }
+    }
+}
